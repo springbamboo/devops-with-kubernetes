db742a8d590d1e5518ca020f9c81059cac14821c
springbamboo <wuchengzhong1125@gmail.com>
2026-08-13 02:37:39 +0900

    exercise 1.5

--- todo-app/index.js
+++ todo-app/index.js
@@ -3,6 +3,10 @@ import 'dotenv/config';
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send(`<p>hello world</p>`);
+});
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server started in port ${PORT}`);
