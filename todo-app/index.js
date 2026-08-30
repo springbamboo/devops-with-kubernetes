e806ae0ae3b8ab3458d8add52ce51a022d175823
springbamboo <wuchengzhong1125@gmail.com>
2026-08-31 02:21:52 +0900

    exercise 1.12

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,13 +1,49 @@
 import express from 'express';
-import 'dotenv/config';
+import fsPromise from 'node:fs/promises';
+import fs from 'node:fs';
+import { Readable } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+import path from 'node:path';
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
-app.get('/', (req, res) => {
-  res.send(`<p>hello world</p>`);
+const filePath = path.join('/usr/src/app', 'files', 'image.jpg');
+// const filePath = __dirname + '/files/image.jpg';
+const TEN_SECONDS = 10 * 1 * 1000;
+
+async function requestAndSaveImage() {
+  const response = await fetch('https://picsum.photos/1200');
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  await pipeline(
+    Readable.fromWeb(response.body),
+    fs.createWriteStream(filePath),
+  );
+}
+
+app.get('/', (_req, res) => {
+  res.sendFile(path.join('/usr/src/app/', 'index.html'));
 });
 
-const PORT = process.env.PORT;
+app.get('/api/image', async (_req, res) => {
+  try {
+    const stats = await fsPromise.stat(filePath);
+    res.sendFile(filePath);
+    if (Date.now() - stats.mtimeMs > TEN_SECONDS) {
+      requestAndSaveImage().catch((err) => console.log(err));
+    }
+  } catch (err) {
+    try {
+      await requestAndSaveImage();
+      res.sendFile(filePath);
+    } catch (downloadErr) {
+      res.status(500).send('error');
+    }
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server started in port ${PORT}`);
 });
